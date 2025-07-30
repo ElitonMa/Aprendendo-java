9edc0bfd40d87fd85bc599597ef23fc0d7365ca5
ELITON MASUREK <eliton_masurek@estudante.sesisenai.org.br>
2025-07-30 13:53:34 -0300

    Adiciona classes Aluno Main e Pessoa para o exercicio 1.

---
+++ Capitulo_4/Exercicio1/Aluno.java
@@ -0,0 +1,9 @@
+package Capitulo_4.Exercicio1;
+
+public class Aluno extends Pessoa {
+    private int  matricula;
+
+    public void exibirDados() {
+        System.out.println("O aluno ");
+    }
+}
---
+++ Capitulo_4/Exercicio1/Pessoa.java
@@ -0,0 +1,6 @@
+package Capitulo_4.Exercicio1;
+
+public class Pessoa {
+    protected String nome;
+    protected int idade;    
+}
